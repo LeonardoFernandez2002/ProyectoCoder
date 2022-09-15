ccf0c4196712f7961c7ae3f670f9a2365bb69106
agustinfernandez <agustinfernan27@gmail.com>
2022-09-15 16:34:41 -0300

    counter

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Title from '../title/Title'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemListContainer = () => {
+
+  const onAdd = (quantity) => {
+    console.log(`Compraste ${quantity} unidades`)
+  }
+
   return (
-    <Title greeting='Pablo'/>
+    <>
+        <Title greeting='Pablo'/>
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+    </>
   )
 }
 
